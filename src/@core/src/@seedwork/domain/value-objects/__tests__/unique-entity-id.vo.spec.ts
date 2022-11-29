e55c3300f7ff3302a680819448be88cf3ba7959d
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-11-29 13:47:33 +0000

    feat: add value object
    add abstract value object\n add value object for entity id

--- src/@core/src/@seedwork/domain/value-objects/__tests__/unique-entity-id.vo.spec.ts
+++ src/@core/src/@seedwork/domain/value-objects/__tests__/unique-entity-id.vo.spec.ts
@@ -1,5 +1,5 @@
-import { InvalidUuidError } from "./../errors";
-import UniqueEntityId from "./unique-entity-id.vo";
+import { InvalidUuidError } from "./../../errors";
+import UniqueEntityId from "./../unique-entity-id.vo";
 import { validate } from "uuid";
 
 const spyValidateMethod = jest.spyOn(
@@ -18,13 +18,13 @@ describe("UniqueEntityId Unit Tests", () => {
     it("should accept a uuid passed in constructor", async () => {
         const uuid = "c0a80121-7ac2-4fd6-8c9a-86f8025400e0";
         const valueObject = new UniqueEntityId(uuid);
-        expect(valueObject.id).toBe(uuid);
+        expect(valueObject.value).toBe(uuid);
         expect(spyValidateMethod).toHaveBeenCalledTimes(1);
     });
 
     it("should create a uuid if no uuid passed in constructor", async () => {
         const valueObject = new UniqueEntityId();
-        expect(validate(valueObject.id)).toBeTruthy();
+        expect(validate(valueObject.value)).toBeTruthy();
         expect(spyValidateMethod).toHaveBeenCalledTimes(1);
     });
 });
--- src/@core/src/@seedwork/domain/value-objects/unique-entity-id.vo.ts
+++ src/@core/src/@seedwork/domain/value-objects/unique-entity-id.vo.ts
@@ -1,14 +1,15 @@
 import { v4 as uuidv4, validate as uuidValidate } from "uuid";
 import { InvalidUuidError } from "../errors";
+import ValueObject from "./value-object";
 
-export class UniqueEntityId {
-    constructor(readonly id?: string) {
-        this.id = id || uuidv4();
+export class UniqueEntityId extends ValueObject<string> {
+    constructor(id?: string) {
+        super(id || uuidv4());
         this.validate();
     }
 
     private validate() {
-        const isValid = uuidValidate(this.id);
+        const isValid = uuidValidate(this._value);
         if (!isValid) {
             throw new InvalidUuidError();
         }
